d36333b35813524b03d13b31dc7b066a183c8380
iAJanahi <54646372+iAJanahi@users.noreply.github.com>
2022-12-13 16:23:00 +0300

    Working on confirm page.

--- RYDE/RYDE/Views/Booking/confirmViewController.swift
+++ RYDE/RYDE/Views/Booking/confirmViewController.swift
@@ -6,13 +6,40 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class confirmViewController: UIViewController {
 
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var toLocation: UILabel!
+    @IBOutlet weak var driverName: UILabel!
+    @IBOutlet weak var goingReturnTime: UILabel!
+    @IBOutlet weak var price: UILabel!
+    
+    
+    
+    
+    var bookedRide: Ride?
+    
+    init?(coder: NSCoder, bookedRide: Ride) {
+        self.bookedRide = bookedRide
+        super.init(coder: coder)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("Init coder has not been implemented!")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let bookedRide = bookedRide {
+            print(bookedRide)
+            userName.text = "Something"
+        }
+        else {
+            print("Booked ride failed!")
+        }
     }
     
 
@@ -25,5 +52,44 @@ class confirmViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    
+    func getUserName() {
+        let ref = Database.database(url: "https://ryde-33483-default-rtdb.europe-west1.firebasedatabase.app/").reference()
+        let uid = Auth.auth().currentUser?.uid
+        
+        guard uid != nil else {
+            print("Error Fetching User ID")
+            return
+        }
+        
+        print(uid!)
+        ref.child("users").observeSingleEvent(of: .value, with: { snapshot in if let result = snapshot.children.allObjects as? [DataSnapshot] {
+                        
+            for child in result.enumerated() {
+                
+                if child.element.hasChild(uid!) {
+                    
+                    let value = child.element.childSnapshot(forPath: uid!).value as? NSDictionary
+                    
+//                    let fName = value?["First Name"] as? String
+//                    let lName = value?["Last Name"] as? String
+//                    let fullName = "\(fName!) \(lName!)"
+//                    self.fullName.text = fullName
+//                    self.emailLabel.text = value? ["Email"] as? String
+                    
+                    
+                }
+            }
+        }
+            
+        })
+            
+    }
+    
+    
+    @IBAction func payWithAppleButtonTapped(_ sender: Any) {
+    }
+    @IBAction func payWithBenefitButtonTapped(_ sender: Any) {
+    }
 }
